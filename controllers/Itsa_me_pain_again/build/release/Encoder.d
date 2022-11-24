b67981e8b5e20a4a557b05b24c6f9d2877aed616
dadihrannar1 <dadihrannar1@gmail.com>
2022-11-24 02:08:27 -0800

    forgot to stage

--- controllers/Itsa_me_pain_again/build/release/Encoder.d
+++
@@ -1,7 +0,0 @@
-build/release/Encoder.o: Encoder.cpp include/Encoder.h include/Point.h \
- /home/dadi/p5-ws/src/packages/UDP/include/UDP_Com.h \
- /home/dadi/p5-ws/src/packages/UDP/include/json.h \
- /usr/local/webots/include/controller/cpp/webots/Display.hpp \
- /usr/local/webots/include/controller/cpp/webots/Device.hpp \
- /usr/local/webots/include/controller/cpp/webots/../../c/webots/types.h \
- /usr/local/webots/include/controller/cpp/webots/ImageRef.hpp
--- controllers/Itsa_me_pain_again/build/release/UDP_Com.d
+++
@@ -1,3 +0,0 @@
-build/release/UDP_Com.o: /home/dadi/p5-ws/src/packages/UDP/UDP_Com.cpp \
- /home/dadi/p5-ws/src/packages/UDP/include/UDP_Com.h \
- /home/dadi/p5-ws/src/packages/UDP/include/json.h
