b43090aac5fa6569833ac181eeeb754b0f27a83a
SimonHaaningAAU <san20@student.aau.dk>
2022-12-02 16:20:58 +0100

    Subscriber works

--- trajectory_generation/src/trajectory_generation_node.cpp
+++ trajectory_generation/src/trajectory_generation_node.cpp
@@ -1,83 +1,155 @@
 #include <armadillo>
 #include <ros/ros.h>
+#include <tf2_ros/transform_listener.h>
+#include <geometry_msgs/TransformStamped.h>
+#include <geometry_msgs/PointStamped.h>
+#include <geometry_msgs/Twist.h>
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 #include <nav_msgs/Odometry.h>
 #include <geometry_msgs/PointStamped.h>
-#include <actionlib/server/action_server.h>
-#include <control_msgs/FollowJointTrajectoryAction.h>
-#include <control_msgs/FollowJointTrajectoryActionGoal.h>
-//#include <trajectory_generation/vision_out.h>
 #include <cmath>
+#include <deque>
 
-int L0 = 250;
-int L1 = 620;
-int L2 = 745;
+/*
+//Class to handle kinematic control and trajectory planning for the robot
+class RobotKinematics{
+private:
+  //Link sizes
+  const int L0 = 250;
+  const int L1 = 620;
+  const int L2 = 745;
 
-void coordinate_callback(const geometry_msgs::PointStamped::ConstPtr &coordinates)
-{
-  ROS_INFO("x coordinate: [%d]", coordinates->point.x);
-  ROS_INFO("y coordinate: [%d]", coordinates->point.y);
-  // ROS_INFO("starting a crack: [%s]", msg-> crack);
-}
+  //Current motor angles
+  double theta1, theta2;
 
-int main(int argc, char **argv)
-{
-  ros::init(argc, argv, "listener");
-  ros::NodeHandle n;
-  ros::Subscriber sub = n.subscribe("crack_coordinates", 1000, coordinate_callback);
+  //Current end effector position
+  double posx, posy;
 
-  ros::spin();
-  return 0;
-}
+  //Desired angles
+  double desired_theta1, desired_theta2;
 
-float *Trajpoints(float *startPos, float *endPos, const int points)
-{
-  float wpts[] = {{startPos[0]}, {startPos[1]}};
-  for (int i = 1; i <= points; i++)
-  {
-    wpts += {{startPos[0] + (startPos[1] - startPos[0]) / points * i}, {endPos[0] + (endPos[1] - endPos[0]) / points * i}};
+  //Desired end effector position
+  double desired_posx, desired_posy;
+
+public:
+  double *forward_kinematics(double theta1, double theta2){
+    double AB[2] = {L1 * cos(theta1), L1 * sin(theta1)};
+    double AD[2] = {L0 - L1 * cos(theta2), L1 * sin(theta2)};
+    double BD[2] = {abs(AD[0] - AB[0]), abs(AD[1] - AB[1])};
+    double L3 = sqrt(pow(BD[0], 2) + pow(BD[1], 2));
+    double phi2 = acos((L3 / 2) / (L2));
+    double phi1 = atan2(AD[1] - AB[1], AD[0] - AB[0]);
+
+    double E[] = {AB[0] + L2 * cos(phi1 + phi2), AB[1] + L2 * sin(phi1 + phi2)};
+    return E;
   }
-  return wpts;
-}
 
-float *avals(float thetastart, float thetaend, float startacc, float endacc, float tf)
-{
-  float a[4] = {};
-  a[0] = thetastart;
-  a[1] = startacc;
-  a[2] = 3 / (pow(tf, 2)) * (thetaend - thetastart);
-  a[3] = -2 / (pow(tf, 3)) * (thetaend - thetastart);
-  return a;
-}
+  double *inverse_kinematics(double xPos, double yPos){
+    double F0 = sqrt(pow(xPos, 2) + pow(yPos, 2));
+    double F1 = sqrt(pow((L0 - xPos), 2) + pow(yPos, 2));
+    double y00 = acos((pow(L1, 2) + pow(F0, 2) - pow(L2, 2)) / (2 * F0 * L1));
+    double y01 = acos((pow(xPos, 2) + pow(F0, 2) - pow(yPos, 2)) / (2 * F0 * xPos));
+    double y10 = acos((pow(L1, 2) + pow(F1, 2) - pow(L2, 2)) / (2 * F1 * L1));
+    double y11 = acos((pow((L0 - xPos), 2) + pow(F1, 2) - pow(yPos, 2)) / (2 * F1 * (L0 - xPos)));
+    double Theta1 = y00 + y01;
+    double Theta2 = y10 + y11;
 
-float *ForKin(float theta1, float theta2)
-{
-  float AB[2] = {L1 * cos(theta1), L1 * sin(theta1)};
-  float AD[2] = {L0 - L1 * cos(theta2), L1 * sin(theta2)};
-  float BD[2] = {abs(AD[0] - AB[0]), abs(AD[1] - AB[1])};
-  float L3 = sqrt(pow(BD[0], 2) + pow(BD[1], 2));
-  // hojde = sqrt((L2^2)-((L3/2)^2));
-  float phi2 = acos((L3 / 2) / (L2));
-  float phi1 = atan2(AD[1] - AB[1], AD[0] - AB[0]);
+    double Angl[] = {Theta1, Theta2};
+    return Angl;
+  }
 
-  // y1 = acos((L3^2)/((2*L3*L2)));
-  // vinkel = acos((dot([1,0],P34))/(1*L3));
-  float E[] = {AB[0] + L2 * cos(phi1 + phi2), AB[1] + L2 * sin(phi1 + phi2)};
-  return E;
-}
+  //Create interpolation points between a start and end position
+  float *interpolated_points(float *startPos, float *endPos, const int points)
+  {
+    float wpts[] = {{startPos[0]}, {startPos[1]}};
+    for (int i = 1; i <= points; i++)
+    {
+      wpts += {{startPos[0] + (startPos[1] - startPos[0]) / points * i}, {endPos[0] + (endPos[1] - endPos[0]) / points * i}};
+    }
+    return wpts;
+  }
 
-float *InvKin(float xPos, float yPos)
-{
-  float F0 = sqrt(pow(xPos, 2) + pow(yPos, 2));
-  float F1 = sqrt(pow((L0 - xPos), 2) + pow(yPos, 2));
-  float y00 = acos((pow(L1, 2) + pow(F0, 2) - pow(L2, 2)) / (2 * F0 * L1));
-  float y01 = acos((pow(xPos, 2) + pow(F0, 2) - pow(yPos, 2)) / (2 * F0 * xPos));
-  float y10 = acos((pow(L1, 2) + pow(F1, 2) - pow(L2, 2)) / (2 * F1 * L1));
-  float y11 = acos((pow((L0 - xPos), 2) + pow(F1, 2) - pow(yPos, 2)) / (2 * F1 * (L0 - xPos)));
-  float Theta1 = y00 + y01;
-  float Theta2 = y10 + y11;
+  //Create polynomium between two points
+  float *avals(float thetastart, float thetaend, float startacc, float endacc, float tf)
+  {
+    float a[4] = {};
+    a[0] = thetastart;
+    a[1] = startacc;
+    a[2] = 3 / (pow(tf, 2)) * (thetaend - thetastart);
+    a[3] = -2 / (pow(tf, 3)) * (thetaend - thetastart);
+    return a;
+  }
+};
+*/
+class CrackMapper{
+private:
+  ros::NodeHandle n;
+  ros::Subscriber point_sub;
+  tf2_ros::Buffer tf2_buffer;
+  tf2_ros::TransformListener tf2_listener;
 
-  float Angl[] = {Theta1, Theta2};
-  return Angl;
+  //List of current trajectories in world coordinates
+  std::deque<geometry_msgs::PointStamped> trajectory_coordinates;
+
+  //Boundary box for camera frame in world coordinate
+  //Pose of camera origin? and width + height
+  double length_camera;
+  double width_camera;
+
+public:
+  CrackMapper(double camera_bbox_length, double camera_bbox_width):
+  tf2_buffer(ros::Duration(600)), tf2_listener(tf2_buffer)
+  {
+    length_camera = camera_bbox_length;
+    width_camera = camera_bbox_width;
+
+    point_sub = n.subscribe<geometry_msgs::PointStamped>("points", 1000, &CrackMapper::coordinate_callback, this);
+  }
+
+  geometry_msgs::TransformStamped camera_transform_stamped; //Transform from current image to world coordinates
+  geometry_msgs::PointStamped recieved_point;
+
+  void coordinate_callback(const geometry_msgs::PointStamped::ConstPtr &coordinate){
+    //Add trajectory coordinate to the back of thelist
+    //trajectory_coordinates.push_back(coordinate -> );
+
+    ROS_INFO("Received point: (%f, %f)", coordinate -> point.x, coordinate -> point.y);
+
+    //Fetch a new transform with the proper time if the current one does not match
+    /*
+    while(recieved_point.header.stamp != camera_transform_stamped.header.stamp){
+      try{
+        //tfBuffer.waitForTranform();
+        //tfListener.waitForTransform();
+
+        camera_transform_stamped = tf2_buffer.lookupTransform("world_frame", "robot_frame", ros::Time(0));
+        
+      }
+      catch (tf2::TransformException &ex) {
+        ROS_WARN("%s",ex.what());
+        ros::Duration(0.1).sleep();
+        continue;
+      }
+    }
+
+    //Transform coordinate into frame
+    geometry_msgs::PointStamped point;
+    tf2_buffer.transform(recieved_point, point, "world_frame");
+    */
+  }
+};
+
+int main(int argc, char **argv){
+  ros::init(argc, argv, "crack_points_listener");
+  ros::NodeHandle n;
+
+  CrackMapper trajectory_map(1.6, 1.024);
+
+  while (n.ok()){
+    ros::spinOnce(); //Spin subscriber once
+    
+  }
+  
+  return 0;
 }
