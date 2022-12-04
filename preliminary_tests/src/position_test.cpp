dbbc19a935dab6a5478e2e200770e2c22019e9dd
Miki0110 <mikipregaard@yahoo.dk>
2022-12-04 12:33:23 -0800

    Completed setting up the test, TODO: FIX THE PID CONTROLLER

--- preliminary_tests/src/position_test.cpp
+++ preliminary_tests/src/position_test.cpp
@@ -5,6 +5,7 @@
 #include <pretests/set_pos.h>
 #include <geometry_msgs/PointStamped.h>
 #include <vector>
+#include <fstream>
 
 #define TIME_STEP 32
 #define N_TESTS 100
@@ -94,34 +95,61 @@ int main(int argc, char **argv)
   w_pos.resize(N_TESTS);
   w_pos = generateCoord(N_TESTS); // generate positions
 
-  float acc_res[N_TESTS][2];
+  float x_acc_res[N_TESTS];
+  float y_acc_res[N_TESTS];
+  float acc_sum[2];
 
   // First call does not work with the GPS so we just send a dummy pos first
   motorSrv.request.theta_1 = M_PI/2;
   motorSrv.request.theta_2 = M_PI/2;
   motorClient.call(motorSrv);
-  ros::Duration(2).sleep();
+  ros::Duration(0.5).sleep();
   ros::spinOnce();
 
   // Run through random positions
   int j=0; //since we are looping trough pointers I need a different counter
   for(auto n=w_pos.begin(); n!=w_pos.end();n=n+2){
     // The *(<variable>+value) is to get the next value in a pointer, sorta like an array
-    invKin(*n, *(n+1));
+    invKin(*n, *(n+1)); // get the motor positions
     motorSrv.request.theta_1 = angl1;
     motorSrv.request.theta_2 = angl2;
-    motorClient.call(motorSrv);
+    motorClient.call(motorSrv); // tell the motors to move
+    // Wait a little so that the arms can keep up
     ros::Duration(0.1).sleep();
     ros::spinOnce();
-    float x_acc = *n-x;
-    float y_acc = *(n+1)-y;
+
+    // save the important data for the results file
+    float x_acc = abs(*n-x);
+    float y_acc = abs(*(n+1)-y);
     ROS_INFO("\naccuracy in x y = %f, %f",x_acc, y_acc);
-    acc_res[j][0] = x_acc;
-    acc_res[j][1] = y_acc;
+    x_acc_res[j] = x_acc;
+    y_acc_res[j] = y_acc;
+    acc_sum[0] += x_acc;
+    acc_sum[1] += y_acc;
     j++;
   }
-    
   
+  //calculate the average
+  float x_acc_avg = acc_sum[0]/N_TESTS;
+  float y_acc_avg = acc_sum[1]/N_TESTS;
+  // Pointers for the max and min values
+  float* xpointer = std::max_element(x_acc_res, x_acc_res+N_TESTS);
+  float* ypointer = std::max_element(y_acc_res, y_acc_res+N_TESTS);
+  float x_acc_max = *(xpointer); float y_acc_max = *(ypointer);
+  std::ofstream resfile; // .txt file to return results
+  // the results file can be found in your catkin workspace
+  resfile.open("accuracy_test.txt"); // This will overwrite old data
+  if (resfile.is_open()){
+    // first we write in the important stuff
+    resfile << "--------SUMMED RESULTS-------" << std::endl;
+    resfile << "Average accuracy:   x=" << x_acc_avg << " y=" << y_acc_avg << std::endl;
+    resfile << "Highest deviations: x=" << x_acc_max << " y=" << y_acc_max << std::endl;
+    resfile << "---------FULL RESULTS--------" << std::endl;
+    for(int i =0; i<=N_TESTS;++i){
+      resfile << "x:" << x_acc_res[i] << " y:" << y_acc_res[i] << std::endl;
+    }
+  }
+  resfile.close();
   
   return 0;
 }
