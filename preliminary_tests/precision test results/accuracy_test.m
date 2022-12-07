efe4cc514f7edb60db9ed044d8fc20218d931d31
Miki Pregaard <48087675+Miki0110@users.noreply.github.com>
2022-12-07 13:24:33 +0100

    Update accuracy_test.m

--- preliminary_tests/precision test results/accuracy_test.m
+++ preliminary_tests/precision test results/accuracy_test.m
@@ -19,19 +19,21 @@ opts = setvaropts(opts, "Var1", "WhitespaceRule", "preserve");
 opts = setvaropts(opts, "Var1", "EmptyFieldRule", "auto");
 
 % Import the data
-accuracytest1 = readtable("C:\Users\mikip\Documents\P5_asphalt_robot\preliminary_tests\precision test results\accuracy_test_1.txt", opts);
-accuracytest2 = readtable("C:\Users\mikip\Documents\P5_asphalt_robot\preliminary_tests\precision test results\accuracy_test_2.txt", opts);
-accuracytest3 = readtable("C:\Users\mikip\Documents\P5_asphalt_robot\preliminary_tests\precision test results\accuracy_test_3.txt", opts);
-accuracytest4 = readtable("C:\Users\mikip\Documents\P5_asphalt_robot\preliminary_tests\precision test results\accuracy_test_4.txt", opts);
-accuracytest5 = readtable("C:\Users\mikip\Documents\P5_asphalt_robot\preliminary_tests\precision test results\accuracy_test_5.txt", opts);
+accuracytest1 = readtable("C:\Users\mikip\Documents\P5_asphalt_robot\preliminary_tests\precision test results\accuracy_test1.txt", opts);
+accuracytest2 = readtable("C:\Users\mikip\Documents\P5_asphalt_robot\preliminary_tests\precision test results\accuracy_test2.txt", opts);
+accuracytest3 = readtable("C:\Users\mikip\Documents\P5_asphalt_robot\preliminary_tests\precision test results\accuracy_test3.txt", opts);
+accuracytest4 = readtable("C:\Users\mikip\Documents\P5_asphalt_robot\preliminary_tests\precision test results\accuracy_test4.txt", opts);
+accuracytest5 = readtable("C:\Users\mikip\Documents\P5_asphalt_robot\preliminary_tests\precision test results\accuracy_test5.txt", opts);
 
 
 %% Extracting important data
 
+alpha = norminv(1-0.00001);
+
 % SET 1
 average1 = sum(accuracytest1.mag)/length(accuracytest1.mag);
 deviation1 = std(accuracytest1.mag);
-significance1 = 3.5*deviation1+average1;
+significance1 = alpha*deviation1+average1;
 highest_deviation1 = max(accuracytest1.mag);
 
 disp(['E = ', num2str(average1),', sigma =',num2str(deviation1),', 95% Significance =',num2str(significance1),', Higest deviation =',num2str(highest_deviation1)])
@@ -39,7 +41,7 @@ disp(['E = ', num2str(average1),', sigma =',num2str(deviation1),', 95% Significa
 % SET 2
 average2 = sum(accuracytest2.mag)/length(accuracytest2.mag);
 deviation2 = std(accuracytest2.mag);
-significance2 = 3.5*deviation2+average2;
+significance2 = alpha*deviation2+average2;
 highest_deviation2 = max(accuracytest2.mag);
 
 disp(['E = ', num2str(average2),', sigma =',num2str(deviation2),', 95% Significance =',num2str(significance2),', Higest deviation =',num2str(highest_deviation2)])
@@ -47,7 +49,7 @@ disp(['E = ', num2str(average2),', sigma =',num2str(deviation2),', 95% Significa
 % SET 3
 average3 = sum(accuracytest3.mag)/length(accuracytest3.mag);
 deviation3 = std(accuracytest3.mag);
-significance3 = 3.5*deviation3+average3;
+significance3 = alpha*deviation3+average3;
 highest_deviation3 = max(accuracytest3.mag);
 
 disp(['E = ', num2str(average3),', sigma =',num2str(deviation3),', 95% Significance =',num2str(significance3),', Higest deviation =',num2str(highest_deviation3)])
@@ -55,7 +57,7 @@ disp(['E = ', num2str(average3),', sigma =',num2str(deviation3),', 95% Significa
 % SET 4
 average4 = sum(accuracytest4.mag)/length(accuracytest4.mag);
 deviation4 = std(accuracytest4.mag);
-significance4 = 3.5*deviation4+average4;
+significance4 = alpha*deviation4+average4;
 highest_deviation4 = max(accuracytest4.mag);
 
 disp(['E = ', num2str(average4),', sigma =',num2str(deviation4),', 95% Significance =',num2str(significance4),', Higest deviation =',num2str(highest_deviation4)])
@@ -63,7 +65,7 @@ disp(['E = ', num2str(average4),', sigma =',num2str(deviation4),', 95% Significa
 % SET 5
 average5 = sum(accuracytest5.mag)/length(accuracytest5.mag);
 deviation5 = std(accuracytest5.mag);
-significance5 = 3.5*deviation5+average5;
+significance5 = alpha*deviation5+average5;
 highest_deviation5 = max(accuracytest5.mag);
 
 disp(['E = ', num2str(average5),', sigma =',num2str(deviation5),', 95% Significance =',num2str(significance5),', Higest deviation =',num2str(highest_deviation5)])
