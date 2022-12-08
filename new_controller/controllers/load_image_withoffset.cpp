1e7ac9320b4c2b489679e1563b7ef90444dcefdc
Miki Pregaard <48087675+Miki0110@users.noreply.github.com>
2022-12-08 07:23:07 -0800

    added offset readability

--- new_controller/controllers/load_image_withoffset.cpp
+++ new_controller/controllers/load_image_withoffset.cpp
@@ -12,6 +12,12 @@ float tickSize = 277.8/100;
 
 float tickPixel = pixelSize/tickSize;
 
+std::vector<std::string> filenames;
+std::vector<int> enctime;
+std::vector<int> encoder1;
+std::vector<int> encoder2;
+
+
 int main(int argc, char **argv){
     ros::init(argc, argv, "a");
     ros::NodeHandle n;
@@ -21,10 +27,7 @@ int main(int argc, char **argv){
    if (jsonfile.is_open()){ //checking whether the file is open
       // define variables
       std::string tp;
-      std::vector<std::string> filenames;
-      std::vector<int> time;
-      std::vector<int> encoder1;
-      std::vector<int> encoder2;
+
       int j = 0;
       while(getline(jsonfile, tp, '"')){ //read data from file object and put it into string.
         if(tp == ", "){continue;} // if it's a ", " go next
@@ -39,7 +42,7 @@ int main(int argc, char **argv){
             filenames.push_back(tp);
             break;
             case 1:
-            time.push_back(std::stoi(tp));
+            enctime.push_back(std::stoi(tp));
             break;
             case 2:
             encoder1.push_back(std::stoi(tp));
@@ -50,7 +53,15 @@ int main(int argc, char **argv){
         }
       }
       jsonfile.close(); //close the file object.
-      ROS_INFO("files");
-      ROS_INFO_STREAM(j);
    }
+int i = 0;
+for(auto n=filenames.begin(); n!=filenames.end();n++){
+    std::istringstream fileStream(*n);
+    std::string f_name;
+    while(getline(fileStream, f_name, '/')){}
+    int offset = encoder1[i]/tickPixel;
+    ROS_INFO_STREAM(offset);
+    i++;
+}
+
 }
