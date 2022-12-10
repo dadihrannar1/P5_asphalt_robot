d524cb4d6c12e555d96a541b3540ccdb0bf78f54
Miki0110 <48087675+Miki0110@users.noreply.github.com>
2022-12-10 16:27:23 +0100

    Corrected the stitcher to better fit webots

--- vision/src/image_stitcher.py
+++ vision/src/image_stitcher.py
@@ -2,8 +2,9 @@ import json
 import cv2
 import pickle
 
-debug = False
+debug = True
 
+desired_w, desired_h = (10000,2000)
 path = r"C:\Users\Muku\OneDrive - Aalborg Universitet\P5 - Crack Robot\Images_lang2"
 
 def resize_image(image, image_name, procent):
@@ -12,7 +13,6 @@ def resize_image(image, image_name, procent):
     cv2.namedWindow(image_name, cv2.WINDOW_NORMAL)
     cv2.resizeWindow(image_name, int(width), int(height))
     cv2.imshow(image_name, image)
-
 
 # calibration values for the camera
 with open(r"calib_matrix.pkl", 'rb') as file:
@@ -40,15 +40,23 @@ for i in range(1030,1050,20):
         img_name = filename[j].split("/")[-1]
         img = cv2.imread(f"{path}/{img_name}")
         frame_corrected = cv2.remap(img, map_x, map_y, cv2.INTER_LINEAR)
-        frame_rotated = cv2.rotate(frame_corrected, cv2.ROTATE_90_CLOCKWISE)
-        images.append(frame_rotated[100:1720, 100:980])
+        images.append(frame_corrected[100:1720, 100:980])
 
     pixelOffset = int(int(encoder1[i+1])/tickPixel)+1
     stitcher.setPanoConfidenceThresh(1)
     result = stitcher.stitch(images)
+
+    h, w = result[1].shape[:2]
+    # increase the size so it fits in webots
+    top = (desired_h - h) // 2
+    bottom = desired_h - h - top
+    left = (desired_w - w) // 2
+    right = desired_w - w - left
+    bordered_image = cv2.copyMakeBorder(result[1], top, bottom, left, right, cv2.BORDER_CONSTANT, value=(0,0,0))
     if debug:
-        for j in range(len(images)):
-            resize_image(images[j],f"cropped{j+1}", 0.4)
+        resize_image(bordered_image,'resized', 0.2)
         resize_image(result[1], "stitched", 0.2)
         cv2.waitKey(0)
-    cv2.imwrite(f'sitched_{i+1}-{i+21}_image.png', result[1])
+    cv2.imwrite(f'sitched_{i+1}-{i+21}_image.png', bordered_image[1])
+    realworldsize = [bordered_image.shape[0]*pixelSize,bordered_image.shape[1]*pixelSize]
+    print(realworldsize)
